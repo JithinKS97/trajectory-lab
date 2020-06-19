570c67645cb2e0e54474351673ef9e161b9a378c
jithinks <jithin.ks@batonsystems.com>
2020-06-19 17:13:01 +0530

    added rnd box

--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
-let dBoxes = [];
+let rndBoxes = [];
 
 function setup() {
   createCanvas(400, 400);
-  dBoxes.push(new DBox({ x: width / 2 + 80, y: height / 2, s: 100 }));
-  dBoxes.push(new DBox({ x: width / 2 - 80, y: height / 2, s: 50 }));
+  rndBoxes.push(new RndBox({ x: width / 2 + 80, y: height / 2, s: 100 }));
+  rndBoxes.push(new RndBox({ x: width / 2 - 80, y: height / 2, s: 50 }));
 }
 
 function draw() {
   background(0);
 
-  dBoxes.forEach((dBox) => {
-    dBox.display();
-    dBox.drag();
+  rndBoxes.forEach((rndBox) => {
+    rndBox.display();
+    rndBox.drag();
   });
 }
